f85a0183ca40c591fd8ba3ccf85d85ade6c08d48
victor <victor.batista.engenharia@gmail.com>
2021-10-15 12:15:41 -0300

    private modifier

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,11 +3,20 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome:String = "Victor"
     var cpf:String = "123.123.123-12"
+
+    /*inner class Endereco{//argumento interno
+        var rua:String = "Rua teste"
+    }*/
 }
 
 fun main(){
     val victor = Pessoa()
 
+    victor.cpf = "4572"//tendo acesso aos dados do objeto
+
+    println(victor)
     println(victor.nome)
     println(victor.cpf)
+
+    //println(victor.Endereco().rua)
 }
